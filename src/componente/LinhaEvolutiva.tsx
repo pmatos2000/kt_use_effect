f6b3cdf61ec1b70e458a9e9b7307ce96176e7dd8
Paulo Henrique Rodrigues de Matos <paulo.matos@dtidigital.com.br>
2023-04-26 09:28:51 -0300

    set state

--- src/componente/LinhaEvolutiva.tsx
+++ src/componente/LinhaEvolutiva.tsx
@@ -12,46 +12,42 @@ const LinhaEvolutiva: FC<LinhaEvolutivaProps> = ({ listaPokemon }) => {
   const [pokemonSelecionado, setPokemonSelecionado] =
     useState<Nullable<Pokemon>>();
 
-    useEffect(() => {
-      if(!listaPokemon.find(pkm => pkm === pokemonSelecionado))
-      {
-        setPokemonSelecionado(null);
-      }
-    }, [listaPokemon, pokemonSelecionado]);
-  
+  if (pokemonSelecionado && !listaPokemon.find((pkm) => pkm === pokemonSelecionado)) {
+    setPokemonSelecionado(null);
+  }
 
+  console.log("Exemplo 4");
 
   return (
     <div className="flex items-center gap-8">
       <div className="flex flex-col items-center gap-4">
         <div className="flex items-center gap-4">
           {listaPokemon.map((pkm, index, lista) => (
-            <>
+            <div className="flex items-center" key={pkm.nDex}>
               <CardPequenoPokemon
-                key={pkm.nDex}
+                
                 pokemon={pkm}
                 selecionado={pkm === pokemonSelecionado}
                 onClick={() => setPokemonSelecionado(pkm)}
               />
-              {index < lista.length - 1 && <h2 key={index}>→</h2>}
-            </>
+              {index < lista.length - 1 && <h2>→</h2>}
+            </div>
           ))}
         </div>
         <div className="flex items-center gap-4">
           {listaPokemon.map((pkm, index, lista) => (
-            <>
+            <div className="flex items-center" key={pkm.nDex}>
               <h1
                 className={
                   pokemonSelecionado?.nDex === pkm.nDex
                     ? "text-yellow-300"
                     : "text-white"
                 }
-                key={pkm.nDex}
               >
                 {pkm.nome}
               </h1>
-              {index < lista.length - 1 && <h2 key={index}>→</h2>}
-            </>
+              {index < lista.length - 1 && <h2>→</h2>}
+            </div>
           ))}
         </div>
       </div>
